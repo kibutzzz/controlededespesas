29f3e02fec4f43b9ef2a41f1714d28f6ec553bd5
Leonardo <leonardo.meinerzramos28@gmail.com>
2019-02-25 13:24:12 -0300

    configurações de resolvers

---
+++ src/main/java/br/inf/safetech/conf/AppWebConfiguration.java
@@ -0,0 +1,45 @@
+package br.inf.safetech.conf;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.format.datetime.DateFormatterRegistrar;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import br.inf.safetech.controllers.HomeController;
+import br.inf.safetech.daos.UsuarioDAO;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses = {HomeController.class, UsuarioDAO.class})
+public class AppWebConfiguration {
+
+	/**
+	 * metodo utilizado pelo spring para obter o resolver
+	 * resolver busca os arquivos
+	 * @return
+	 */
+	@Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+       
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+	
+	@Bean
+	public FormattingConversionService mvcConversionService() {
+		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+		DateFormatterRegistrar registrar = new DateFormatterRegistrar();
+		registrar.setFormatter(new DateFormatter("yyyy-MM-dd"));
+		registrar.registerFormatters(conversionService);
+		return conversionService;
+		
+	}
+	
+}
