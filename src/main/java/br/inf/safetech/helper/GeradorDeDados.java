e09adfa4f44d9f2165d3a383c84360f4c78c5aef
Leonardo <leonardo.meinerzramos28@gmail.com>
2019-02-25 13:32:40 -0300

    helper class para criar clientes e usuarios imaginarios facilitando o desenvolvimento

---
+++ src/main/java/br/inf/safetech/helper/GeradorDeDados.java
@@ -0,0 +1,58 @@
+package br.inf.safetech.helper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.inf.safetech.model.Cliente;
+import br.inf.safetech.model.SituacaoUsuario;
+import br.inf.safetech.model.TipoUsuario;
+import br.inf.safetech.model.Usuario;
+
+public class GeradorDeDados {
+
+	public static List<Cliente> gerarClientes() {
+		List<Cliente> clientes = new ArrayList<Cliente>();
+
+		for (int i = 0; i < 5; i++) {
+			clientes.add(new Cliente());
+		}
+
+		clientes.get(0).setNome("Mercado Guarani");
+		clientes.get(1).setNome("Rissul");
+		clientes.get(2).setNome("Xis do Vini");
+		clientes.get(3).setNome("Bottero");
+		clientes.get(4).setNome("Azaleia");
+
+		return clientes;
+	}
+
+	public static List<Usuario> gerarUsuarios() {
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+
+		usuarios.add(criaUsuario("leonardo.ramos", "Leonardo Meinerz Ramos", "leonardo123", SituacaoUsuario.ATIVO,
+				TipoUsuario.COLABORADOR));
+		usuarios.add(
+				criaUsuario("mateus.santos", "Mateus Santos", "senha", SituacaoUsuario.ATIVO, TipoUsuario.COLABORADOR));
+		usuarios.add(criaUsuario("marcia", "marcia", "admin", SituacaoUsuario.ATIVO, TipoUsuario.ADMIN));
+
+		usuarios.add(criaUsuario("usuario.inativo", "usuario demitido", "123", SituacaoUsuario.INATIVO,
+				TipoUsuario.COLABORADOR));
+		usuarios.add(criaUsuario("admin.inativo", "admin demitido", "4456", SituacaoUsuario.INATIVO,
+				TipoUsuario.ADMIN));
+
+		return usuarios;
+	}
+
+	private static Usuario criaUsuario(String login, String nome, String senha, SituacaoUsuario situacao,
+			TipoUsuario tipo) {
+		Usuario usuario = new Usuario();
+		usuario.setLogin(login);
+		usuario.setNome(nome);
+		usuario.setSenha(senha);
+		usuario.setSituacao(situacao);
+		usuario.setTipo(tipo);
+
+		return usuario;
+	}
+
+}
