ed3b40b55df4a1b4e16421fc24a0c6b0b8b436b1
Leonardo <leonardo.meinerzramos28@gmail.com>
2019-02-26 15:25:45 -0300

    implementado metodo para buscar conta pelo id com as movimentações, listar contas por usuario e fazer update da conta

--- src/main/java/br/inf/safetech/daos/ContaDespesaDAO.java
+++ src/main/java/br/inf/safetech/daos/ContaDespesaDAO.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.inf.safetech.model.ContaDespesa;
+import br.inf.safetech.model.Usuario;
 
 @Repository
 @Transactional
@@ -16,13 +17,28 @@ public class ContaDespesaDAO {
 
 	@PersistenceContext
 	private EntityManager manager;
-	
+
 	public void gravar(ContaDespesa conta) {
 		manager.persist(conta);
 	}
-	
-	public List<ContaDespesa> listar(){
+
+	public List<ContaDespesa> listar() {
 		return manager.createQuery("select c from ContaDespesa c", ContaDespesa.class).getResultList();
 	}
+
+	public ContaDespesa buscarContaPeloId(Integer id) {
+
+		return manager.createQuery("select c from ContaDespesa c left join fetch c.movimentacoes where c.id = :pId", ContaDespesa.class)
+				.setParameter("pId", id).getSingleResult();
+	}
+
+	public List<ContaDespesa> listarContaPorUsuario(Usuario usuario) {
+		return manager.createQuery("select c from ContaDespesa c where c.usuario = :pUsuario", ContaDespesa.class)
+				.setParameter("pUsuario", usuario).getResultList();
+	}
 	
+	public void mesclar(ContaDespesa conta) {
+		manager.merge(conta);
+	}
+
 }
