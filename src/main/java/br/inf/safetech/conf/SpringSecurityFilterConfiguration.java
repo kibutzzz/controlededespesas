1dd8bd2083239d7f2eb728feeba1811161a4fd90
Leonardo <leonardo.meinerzramos28@gmail.com>
2019-03-05 14:07:22 -0300

    classe necessária para a inicialização do spring security

---
+++ src/main/java/br/inf/safetech/conf/SpringSecurityFilterConfiguration.java
@@ -0,0 +1,9 @@
+package br.inf.safetech.conf;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityFilterConfiguration extends AbstractSecurityWebApplicationInitializer{
+	
+	
+
+}
