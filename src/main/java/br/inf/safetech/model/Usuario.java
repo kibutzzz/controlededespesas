4b8b8fd76f34fd3973995b157463ffcb706f3662
Leonardo <leonardo.meinerzramos28@gmail.com>
2019-03-05 13:58:42 -0300

    implementada a interface requisitada pelo spring security

--- src/main/java/br/inf/safetech/model/Usuario.java
+++ src/main/java/br/inf/safetech/model/Usuario.java
@@ -1,59 +1,130 @@
 package br.inf.safetech.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
-public class Usuario {
-	
-	@Id @GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "id_Sequence")
+public class Usuario implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_Sequence")
 	@SequenceGenerator(name = "id_Sequence", sequenceName = "ID_SEQ_USUARIO")
 	private int id;
 	private String nome;
 	private String login;
 	private String senha;
 	private TipoUsuario tipo;
-	private SituacaoUsuario situacao;		
-	
-	
+	private SituacaoUsuario situacao;
+
+	@ManyToMany(fetch=FetchType.EAGER)
+	private List<Role> roles = new ArrayList<Role>();
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getLogin() {
 		return login;
 	}
+
 	public void setLogin(String login) {
 		this.login = login;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
 	public TipoUsuario getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(TipoUsuario tipo) {
 		this.tipo = tipo;
 	}
+
 	public SituacaoUsuario getSituacao() {
 		return situacao;
 	}
+
 	public void setSituacao(SituacaoUsuario situacao) {
 		this.situacao = situacao;
+	}
+
+	
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+				
+		return this.roles;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.login;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true ;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return this.situacao == SituacaoUsuario.ATIVO;
 	}
 
 //	@Override
@@ -62,6 +133,5 @@ public class Usuario {
 //				+ ", situacao=" + situacao + "]";
 //	}
 //	
-	
-	
+
 }
