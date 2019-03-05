55614e08c152f80f04a45a6645429c8bda7b35dc
Leonardo <leonardo.meinerzramos28@gmail.com>
2019-03-05 16:06:11 -0300

    implementada a interface requisitada pelo spring security

--- src/main/java/br/inf/safetech/model/TipoUsuario.java
+++ src/main/java/br/inf/safetech/model/TipoUsuario.java
@@ -1,5 +1,13 @@
 package br.inf.safetech.model;
 
-public enum TipoUsuario {
-	ADMIN, COLABORADOR
+import org.springframework.security.core.GrantedAuthority;
+
+public enum TipoUsuario implements GrantedAuthority {
+	ADMIN, COLABORADOR;
+
+	@Override
+	public String getAuthority() {
+
+		return "ROLE_" + this.toString();
+	}
 }
