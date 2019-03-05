b70500fadb7356ca475f827f4e1896212f476a7a
Leonardo <leonardo.meinerzramos28@gmail.com>
2019-03-05 14:09:03 -0300

    Classe da Role

---
+++ src/main/java/br/inf/safetech/model/Role.java
@@ -0,0 +1,34 @@
+package br.inf.safetech.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+public class Role implements GrantedAuthority{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String getAuthority() {
+		return this.nome;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "[ nome: " + this.nome + "]";
+	}
+}
