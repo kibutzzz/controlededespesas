490d51f91fea6387fc8781f27b4ca21b7e85864b
Leonardo <leonardo.meinerzramos28@gmail.com>
2019-03-05 16:05:18 -0300

    metodos adaptados para não utilizar roles

--- src/main/java/br/inf/safetech/helper/GeradorDeDados.java
+++ src/main/java/br/inf/safetech/helper/GeradorDeDados.java
@@ -3,10 +3,8 @@ package br.inf.safetech.helper;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import br.inf.safetech.daos.RoleDAO;
 import br.inf.safetech.model.Cliente;
 import br.inf.safetech.model.Role;
 import br.inf.safetech.model.SituacaoUsuario;
@@ -40,32 +38,30 @@ public class GeradorDeDados {
 		return roles;
 	}
 
-	public List<Usuario> gerarUsuarios(List<Role> roles) {
+	public List<Usuario> gerarUsuarios() {
 		List<Usuario> usuarios = new ArrayList<Usuario>();
 
 		usuarios.add(criaUsuario("leonardo.ramos", "Leonardo Meinerz Ramos", "leonardo123", SituacaoUsuario.ATIVO,
-				TipoUsuario.COLABORADOR, roles));
-		usuarios.add(criaUsuario("mateus.santos", "Mateus Santos", "senha", SituacaoUsuario.ATIVO,
-				TipoUsuario.COLABORADOR, null));
-		usuarios.add(criaUsuario("marcia", "marcia", "admin", SituacaoUsuario.ATIVO, TipoUsuario.ADMIN, null));
+				TipoUsuario.COLABORADOR));
+		usuarios.add(
+				criaUsuario("mateus.santos", "Mateus Santos", "senha", SituacaoUsuario.ATIVO, TipoUsuario.COLABORADOR));
+		usuarios.add(criaUsuario("marcia", "marcia", "admin", SituacaoUsuario.ATIVO, TipoUsuario.ADMIN));
 
 		usuarios.add(criaUsuario("usuario.inativo", "usuario demitido", "123", SituacaoUsuario.INATIVO,
-				TipoUsuario.COLABORADOR, null));
-		usuarios.add(criaUsuario("admin.inativo", "admin demitido", "4456", SituacaoUsuario.INATIVO, TipoUsuario.ADMIN,
-				null));
+				TipoUsuario.COLABORADOR));
+		usuarios.add(
+				criaUsuario("admin.inativo", "admin demitido", "4456", SituacaoUsuario.INATIVO, TipoUsuario.ADMIN));
 
 		return usuarios;
 	}
 
-	private Usuario criaUsuario(String login, String nome, String senha, SituacaoUsuario situacao, TipoUsuario tipo,
-			List<Role> roles) {
+	private Usuario criaUsuario(String login, String nome, String senha, SituacaoUsuario situacao, TipoUsuario tipo) {
 		Usuario usuario = new Usuario();
 		usuario.setLogin(login);
 		usuario.setNome(nome);
 		usuario.setSenha(new BCryptPasswordEncoder().encode(senha));
 		usuario.setSituacao(situacao);
 		usuario.setTipo(tipo);
-		usuario.setRoles(roles);
 		return usuario;
 	}
 
