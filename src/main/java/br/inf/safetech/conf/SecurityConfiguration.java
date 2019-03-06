7ae2eab16da83c362026ea29662712c6b3f1d907
Leonardo <leonardo.meinerzramos28@gmail.com>
2019-03-06 15:12:54 -0300

    adicionado caminho para logout da sessão e caminho default pós login

--- src/main/java/br/inf/safetech/conf/SecurityConfiguration.java
+++ src/main/java/br/inf/safetech/conf/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import br.inf.safetech.daos.UsuarioDAO;
 import br.inf.safetech.model.TipoUsuario;
@@ -34,7 +35,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/colaborador/**").hasRole(TipoUsuario.COLABORADOR.toString())
 				.antMatchers("/**").permitAll()
 				.anyRequest().authenticated()
-				.and().formLogin();
+				.and().formLogin().defaultSuccessUrl("/", false)
+				.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 	}
 
 	/**
