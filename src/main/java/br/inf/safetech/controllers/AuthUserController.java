8d72860ab74ef47d59a4774c0f55f358da6d5508
Leonardo <leonardo.meinerzramos28@gmail.com>
2019-03-11 16:00:52 -0300

    adicionado controller com os dados da pagina de login

---
+++ src/main/java/br/inf/safetech/controllers/AuthUserController.java
@@ -0,0 +1,19 @@
+package br.inf.safetech.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AuthUserController {
+	
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public ModelAndView loginForm() {
+		
+		
+		return new ModelAndView("login");
+	}
+
+}
