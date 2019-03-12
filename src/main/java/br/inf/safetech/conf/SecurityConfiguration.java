45079c8fe0f2633c8f32de696e61641934770904
Leonardo <leonardo.meinerzramos28@gmail.com>
2019-03-12 14:30:29 -0300

    configuração do spring security para desbloquear os arquivos na pasta /resources

--- src/main/java/br/inf/safetech/conf/SecurityConfiguration.java
+++ src/main/java/br/inf/safetech/conf/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package br.inf.safetech.conf;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -45,6 +46,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(usuarioDao).passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		 web.ignoring().antMatchers("/resources/**");
 	}
 
 	
