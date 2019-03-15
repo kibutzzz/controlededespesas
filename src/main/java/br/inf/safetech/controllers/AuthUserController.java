079dc591bc4d3b82ac46019eaaeb084d5a3ad58f
Leonardo <leonardo.meinerzramos28@gmail.com>
2019-03-15 16:54:00 -0300

    Adicionado mapping de acesso negado (HTTP/403)

--- src/main/java/br/inf/safetech/controllers/AuthUserController.java
+++ src/main/java/br/inf/safetech/controllers/AuthUserController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.inf.safetech.helper.StatusInfo;
+import br.inf.safetech.helper.StatusType;
 
 @Controller
 public class AuthUserController {
@@ -12,8 +16,17 @@ public class AuthUserController {
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public ModelAndView loginForm() {
 		
-		
+	
 		return new ModelAndView("login");
+	}
+	
+	@RequestMapping("/acessoNegado")
+	public ModelAndView acessoNegado(RedirectAttributes redirectAtriAttributes) {
+		
+		redirectAtriAttributes.addFlashAttribute("status", new StatusInfo(StatusType.ALERTA, 
+			"Você não tem permissão para logar nesta página. Faça login para poder acessá-la."));
+		
+		return new ModelAndView("acessoNegado");
 	}
 
 }
