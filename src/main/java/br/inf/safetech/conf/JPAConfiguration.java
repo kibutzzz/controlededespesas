322308c43380bd078928606dd827ed03e5e91029
Leonardo <leonardo.meinerzramos28@gmail.com>
2019-03-18 08:36:20 -0300

    tarefa adicionada

--- src/main/java/br/inf/safetech/conf/JPAConfiguration.java
+++ src/main/java/br/inf/safetech/conf/JPAConfiguration.java
@@ -12,50 +12,50 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
-
 @EnableTransactionManagement
 public class JPAConfiguration {
 
 	private static final String USERNAME = "leonardo";
 	private static final String PASSWORD = "password";
 	private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-	private static final String DRIVER_CLASS_NAME = "oracle.jdbc.OracleDriver"; 
-	
+	private static final String DRIVER_CLASS_NAME = "oracle.jdbc.OracleDriver";
+
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-		LocalContainerEntityManagerFactoryBean factoryBean = 
-				new LocalContainerEntityManagerFactoryBean();
-		
+		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+
 		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		factoryBean.setJpaVendorAdapter(vendorAdapter);
-		
+
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setUsername(USERNAME);
 		dataSource.setPassword(PASSWORD);
 		dataSource.setUrl(URL);
-		dataSource.setDriverClassName(DRIVER_CLASS_NAME); 
-		
+		dataSource.setDriverClassName(DRIVER_CLASS_NAME);
+
 		factoryBean.setDataSource(dataSource);
-		
-		
+
 		Properties props = new Properties();
 		props.setProperty("hibernate.dialect", "org.hibernate.dialect.OracleDialect");
 		props.setProperty("hibernate.show_sql", "true");
 		props.setProperty("hibernate.hbm2ddl.auto", "create-drop");
 		
+//		TODO resolver problemas de encoding
+// 		props.setProperty("hibernate.connection.characterEncoding", "UTF-8");
+//		props.setProperty("hibernate.connection.CharSet", "utf-8");
+//		props.setProperty("hibernate.connection.useUnicode", "true");
+
 		factoryBean.setJpaProperties(props);
-		
+
 		factoryBean.setPackagesToScan("br.inf.safetech.model");
-	
+
 		return factoryBean;
-		
+
 	}
-	
-	
+
 	@Bean
-	public JpaTransactionManager transactionManager(EntityManagerFactory emf){
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
 		return new JpaTransactionManager(emf);
 	}
-	
+
 }
