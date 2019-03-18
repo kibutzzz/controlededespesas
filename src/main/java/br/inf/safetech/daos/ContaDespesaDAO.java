3159b4d07773ef869a17e39c9e790abf1aa166a0
Leonardo <leonardo.meinerzramos28@gmail.com>
2019-03-18 11:27:33 -0300

    filtro com CriteriaAPI

--- src/main/java/br/inf/safetech/daos/ContaDespesaDAO.java
+++ src/main/java/br/inf/safetech/daos/ContaDespesaDAO.java
@@ -1,15 +1,25 @@
 package br.inf.safetech.daos;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.inf.safetech.formwrapper.FiltroContaWrapper;
+import br.inf.safetech.model.Cliente;
 import br.inf.safetech.model.ContaDespesa;
+import br.inf.safetech.model.SituacaoConta;
 import br.inf.safetech.model.Usuario;
 
 @Repository
@@ -19,7 +29,7 @@ public class ContaDespesaDAO {
 	@PersistenceContext
 	private EntityManager manager;
 
-	public void gravar(ContaDespesa conta) throws RuntimeException{
+	public void gravar(ContaDespesa conta) throws RuntimeException {
 		manager.persist(conta);
 	}
 
@@ -44,11 +54,10 @@ public class ContaDespesaDAO {
 				ContaDespesa.class).setParameter("pId", id).getSingleResult();
 	}
 
-	
 	/**
 	 * busca todas as conta associadas ao usuario passado como argumento
 	 * 
-	 * @param usuario 
+	 * @param usuario
 	 * @return lista de contas associada ao usuario
 	 */
 	public List<ContaDespesa> listarContaPorUsuario(Usuario usuario) {
@@ -63,6 +72,58 @@ public class ContaDespesaDAO {
 	 */
 	public void mesclar(ContaDespesa conta) throws RuntimeException {
 		manager.merge(conta);
+	}
+
+	public List<ContaDespesa> buscarContaFiltrada(FiltroContaWrapper wrapper) {
+
+		CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
+		CriteriaQuery<ContaDespesa> query = criteriaBuilder.createQuery(ContaDespesa.class);
+
+		Root<ContaDespesa> root = query.from(ContaDespesa.class);
+		List<Predicate> predicates = new ArrayList<Predicate>();
+
+		Path<String> usuarioPath = root.<Usuario>get("usuario").<String>get("id");
+		Path<String> clientePath = root.<Cliente>get("cliente").<String>get("id");
+		Path<Calendar> dataInicioPath = root.<Calendar>get("dataInicio");
+		Path<Calendar> dataFimPath = root.<Calendar>get("dataFim");
+		Path<SituacaoConta> situacaoPath = root.<SituacaoConta>get("situacao");
+		
+		if(!(wrapper.getColaborador() == null)) {
+			System.out.println("filtrando colaborador " + wrapper.getColaborador());
+			
+			Predicate usuarioIgual = criteriaBuilder.equal(usuarioPath, wrapper.getColaborador().getId());
+			predicates.add(usuarioIgual);
+		}
+		
+		if(! (wrapper.getCliente() == null)) {
+			System.out.println("filtrando cliente " + wrapper.getCliente());
+			Predicate clienteIgual = criteriaBuilder.equal(clientePath, wrapper.getCliente().getId());
+			predicates.add(clienteIgual);
+		}
+		
+		if(!(wrapper.getDataInicio() == null)) {
+			System.out.println("filtrando dataInicio");
+			Predicate dataInicioMaiorQue = criteriaBuilder.greaterThanOrEqualTo(dataInicioPath, wrapper.getDataInicio());
+			predicates.add(dataInicioMaiorQue);
+		}
+		
+		if(!(wrapper.getDataInicio() == null)) {
+			System.out.println("filtrando dataFim");
+			Predicate dataFimMaiorQue = criteriaBuilder.greaterThanOrEqualTo(dataFimPath, wrapper.getDataFim());
+			predicates.add(dataFimMaiorQue);
+		}
+		
+		if(!(wrapper.getSituacao() == null)) {
+			System.out.println("filtrando situação");
+			Predicate situacaoIgual = criteriaBuilder.equal(situacaoPath, wrapper.getSituacao());
+			predicates.add(situacaoIgual);
+		}
+		
+
+		query.where((Predicate[]) predicates.toArray(new Predicate[0]));
+		List<ContaDespesa> contas = manager.createQuery(query).getResultList();
+		System.out.println(contas.size());
+		return contas;
 	}
 
 }
