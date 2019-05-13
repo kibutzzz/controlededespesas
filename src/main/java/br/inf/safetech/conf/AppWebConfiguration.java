9669d4eeab8a319d8b163f9204c6ad3081db5678
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2019-05-13 16:29:12 -0300

    buscando configuração do arquivo externo

--- src/main/java/br/inf/safetech/conf/AppWebConfiguration.java
+++ src/main/java/br/inf/safetech/conf/AppWebConfiguration.java
@@ -1,10 +1,17 @@
 package br.inf.safetech.conf;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Properties;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,31 +22,38 @@ import br.inf.safetech.controllers.HomeController;
 import br.inf.safetech.daos.UsuarioDAO;
 
 @EnableWebMvc
-@ComponentScan(basePackageClasses = {HomeController.class, UsuarioDAO.class})
-@EnableTransactionManagement(proxyTargetClass=true)
-public class AppWebConfiguration extends WebMvcConfigurerAdapter{
+@ComponentScan(basePackageClasses = { HomeController.class, UsuarioDAO.class })
+@EnableTransactionManagement(proxyTargetClass = true)
+public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 
 	/**
-	 * metodo utilizado pelo spring para obter o resolver
-	 * resolver busca os arquivos
+	 * metodo utilizado pelo spring para obter o resolver resolver busca os arquivos
+	 * 
 	 * @return
 	 */
 	@Bean
-    public InternalResourceViewResolver internalResourceViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-       
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 
-        return resolver;
-    }
-	
-	
-	
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+
+		return resolver;
+	}
+
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
-	} 
-	
-	
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() throws IOException {
+		PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+		Resource resource = new UrlResource("file:C:\\Apache Software Foundation\\apache-tomcat-9.0.19\\webapps\\conf\\controlededespesas-config.properties");
+		Properties properties = PropertiesLoaderUtils.loadProperties(resource);
+		
+		pspc.setProperties(properties);
+		return pspc ;
+	}
+
 }
--- src/main/java/br/inf/safetech/conf/JPAConfiguration.java
+++ src/main/java/br/inf/safetech/conf/JPAConfiguration.java
@@ -1,10 +1,16 @@
 package br.inf.safetech.conf;
 
+import java.io.IOException;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -15,25 +21,36 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class JPAConfiguration {
 
-	private static final String USERNAME = "leonardotmp";
-	private static final String PASSWORD = "safetech";
-	private static final String HOST = "172.20.1.40";
-	private static final String PORT = "1521";
-	private static final String SERVICE_NAME = "tsft";
-	private static final String URL = "jdbc:oracle:thin:@" + HOST + ":" + PORT + ":" + SERVICE_NAME;
+	@Value("${database.user}")
+	String user;
+	@Value("${database.password}")
+	String password;
+	@Value("${database.host}")
+	String host;
+	@Value("${database.port}")
+	String port;
+	@Value("${database.serviceName}")
+	String serviceName;	
+	
+	private static String url; 
 	private static final String DRIVER_CLASS_NAME = "oracle.jdbc.OracleDriver";
 
 	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws IOException {
 		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
 
 		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		factoryBean.setJpaVendorAdapter(vendorAdapter);
 
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setUsername(USERNAME);
-		dataSource.setPassword(PASSWORD);
-		dataSource.setUrl(URL);
+		
+			
+		
+		url = "jdbc:oracle:thin:@" + host + ":" + port + ":" + serviceName;
+		System.out.println(url);
+		dataSource.setUsername(user);
+		dataSource.setPassword(password);
+		dataSource.setUrl(url);
 		dataSource.setDriverClassName(DRIVER_CLASS_NAME);
 
 		factoryBean.setDataSource(dataSource);
